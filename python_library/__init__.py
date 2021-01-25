75e98ba111c21f3e3d0da86a5ecfdbe391638405
root <aaa>
2021-01-26 01:13:06 +0800

    add

--- python_library/__init__.py
+++ python_library/__init__.py
@@ -1,1 +1,5 @@
 from .core import *
+from .channel import *
+from .device_manager import *
+from .tranceiver import *
+
--- python_library/core/Signal.py
+++ python_library/core/Signal.py
@@ -1,19 +1,20 @@
-from ..device_manager import cuda_number,device_selection
+from ..device_manager import cuda_number, device_selection
 from dataclasses import dataclass
+
 
 @dataclass
 class SignalSetting:
-    center_freq:float
-    sps_in_fiber:int
-    sps_dsp:int = 2
-    device:str='cpu'
+    center_freq: float
+    sps_in_fiber: int
+    sps_dsp: int = 2
+    device: str = 'cpu'
 
     #####QAM Signal###
-    symbol_rate:float=35e9
-    symbol_number:int=65536
-    qam_order:int=16
-    need_init:bool=True
-    pol_number:int=2
+    symbol_rate: float = 35e9
+    symbol_number: int = 65536
+    qam_order: int = 16
+    need_init: bool = True
+    pol_number: int = 2
 
 
 class Signal(object):
@@ -22,7 +23,7 @@ class Signal(object):
                  samples,
                  center_freq,
                  sps_in_fiber,
-                 sps_dsp = 2,
+                 sps_dsp=2,
                  device='cpu'
                  ):
 
@@ -34,10 +35,10 @@ class Signal(object):
 
         self.to(device)
 
-    def to(self,device):
+    def to(self, device):
 
-        @device_selection(device,provide_backend=True)
-        def to_(backend,signal_obj):
+        @device_selection(device, provide_backend=True)
+        def to_(backend, signal_obj):
             signal_obj.samples = backend.asarray(signal_obj.samples)
             signal_obj.device = device
             return signal_obj
@@ -49,14 +50,13 @@ class Signal(object):
 
     def normalize(self):
 
-        @device_selection(self.device,provide_backend=True)
-        def normalize_(backend,signal_obj):
-            factor = backend.mean(backend.abs(signal_obj[:])**2,axis=-1,keedims=True)
-            signal_obj[:] = signal_obj[:]/backend.sqrt(factor)
+        @device_selection(self.device, provide_backend=True)
+        def normalize_(backend, signal_obj):
+            factor = backend.mean(backend.abs(signal_obj[:]) ** 2, axis=-1, keedims=True)
+            signal_obj[:] = signal_obj[:] / backend.sqrt(factor)
             return signal_obj
 
         return normalize_(self)
-
 
     def __getitem__(self, item):
         return self.samples[item]
@@ -65,25 +65,26 @@ class Signal(object):
         self.samples[key] = value
 
     def power(self):
-        @device_selection(self.device,True)
-        def power_(backend,signal_obj):
-            power = backend.mean(backend.abs(signal_obj)**2,axis=-1)
-            power_dbm = 10*backend.log10(power*1000)
+        @device_selection(self.device, True)
+        def power_(backend, signal_obj):
+            power = backend.mean(backend.abs(signal_obj) ** 2, axis=-1)
+            power_dbm = 10 * backend.log10(power * 1000)
 
             print(f'{power[0]}:.4f W, {power[1]}:.4f W')
             print(f'{power_dbm[0]}:.4f dBm, {power[1]}:.4f dBm')
 
         power_(self)
 
+
 class QamSignal(Signal):
 
     def __init__(self,
                  symbol_rate,
                  qam_order,
-                 signal_setting:SignalSetting
+                 signal_setting: SignalSetting
                  ):
         import numpy as np
-        from .constl import cal_symbols_qam,cal_scaling_factor_qam
+        from .constl import cal_symbols_qam, cal_scaling_factor_qam
         self.symbol_rate = signal_setting.symbol_rate
         self.symbol_number = signal_setting.symbol_number
         self.qam_order = signal_setting.qam_order
@@ -97,11 +98,24 @@ class QamSignal(Signal):
             self.map()
             samples = np.zeros_like((self.pol_number, self.symbol_number * self.sps_dsp), dtype=np.complex)
             samples[:, ::self.sps_dsp] = self.symbol
-            super(QamSignal, self).__init__(samples=samples,center_freq=signal_setting.center_freq,sps_in_fiber=signal_setting.sps_in_fiber,sps_dsp=signal_setting.sps_dsp,device=signal_setting.device)
+            super(QamSignal, self).__init__(samples=samples, center_freq=signal_setting.center_freq,
+                                            sps_in_fiber=signal_setting.sps_in_fiber, sps_dsp=signal_setting.sps_dsp,
+                                            device=signal_setting.device)
 
     def map(self):
         from .constl import generate_mapping, map
         _, encoding = generate_mapping(self.qam_order)
         self.symbol = map(self.bit_sequence, encoding=encoding, M=self.qam_order)
+
+
+class WdmSignal(Signal):
+
+    def __init__(self, symbols, samples, freqes, center_freq, sps_in_fiber, sps_dsp, device):
+        import numpy as np
+        self.symbols = symbols
+        self.freq = freqes
+        self.relative_freq = np.array(self.freq) - center_freq
+        super(WdmSignal, self).__init__(samples=samples, center_freq=center_freq, sps_in_fiber=sps_in_fiber,
+                                        sps_dsp=sps_dsp, device=device)
 
 
