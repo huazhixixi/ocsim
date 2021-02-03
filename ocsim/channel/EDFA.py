47ebe0d3d9c667ca0c0c5266e02a14d2c4ae0b8d
hello <huazhi.lun@sjtu.edu.cn>
2021-02-03 19:02:45 +0800

    add edfa and WSS,please verify

---
+++ ocsim/channel/EDFA.py
@@ -0,0 +1,46 @@
+import numpy as np
+from ..device_manager import device_selection
+from scipy.constants import h,c
+
+
+class EDFA:
+
+    def calc_noise_power(self,wavelength,fs):
+        ase_psd = (h*c/wavelength)*(self.gain_linear *10**(self.nf/10)-1)/2
+        noise_power = ase_psd * fs
+        return noise_power
+
+    @property
+    def gain_linear(self):
+        return 10**(self.gain/10)
+
+    
+    def noise_sequence(self,signal):
+        noise_power = self.calc_noise_power(c/signal.center_freq,signal.fs)
+        @device_selection(signal.device,True) 
+        def noise_sequence_real(backend):
+            
+            noise_sequence = backend.sqrt(noise_power/2) * (backend.random.randn(*signal.shape)+1j * backend.random.randn(*signal.shape))
+            return noise_sequence
+
+        return noise_sequence_real()
+
+class ConstantGainEDFA(EDFA):
+
+    def __init__(self,gain,nf):
+        self.gain = gain
+        self.nf = nf
+
+    def __core(self,signal):
+       
+        @device_selection(signal.device,True)
+        def core_real(backend):
+            noise_sequence = self.noise_sequence(signal)
+            signal[:] = signal[:] * backend.sqrt(self.gain_linear)*signal[:]
+            signal[:] = signal[:] + noise_sequence
+            return signal
+
+        return core_real()
+
+    def __call__(self,signal):
+        return self.__core(signal)
---
+++ ocsim/channel/ROADM.py
@@ -0,0 +1,67 @@
+import numpy as np
+from ..device_manager import device_selection
+
+
+class WSS:
+
+    def __init__(self,center_frequency,bandwidth,otf):
+
+        self.center_freq = center_frequency
+        self.bandwidth = bandwidth
+        self.otf = otf
+
+        self.H = None
+
+    
+    def get_transfer_function(self, freq_vector,device):
+
+        @device_selection(device,True)
+        def get_transfer_function_real(backend):
+            
+            if 'cuda' in device:
+                from cupyx.scipy.special import erf
+            else:
+                from scipy.special import erf
+            delta = self.otf / 2 / backend.sqrt(2 * backend.log(2))
+
+            H = 0.5 * delta * backend.sqrt(2 * backend.pi) * (
+                erf((self.bandwidth / 2 - (freq_vector - self.center_freq)) / backend.sqrt(2) / delta) - erf(
+            (-self.bandwidth / 2 - (freq_vector - self.center_freq)) / backend.sqrt(2) / delta))
+
+            H = H / np.max(H)
+
+            self.H = H
+
+    def __core(self,signal):
+        
+        @device_selection(signal.device,True)
+        def core_real(backend):
+            freq = backend.fft.fftfreq(signal.shape[1],1/signal.fs)
+            self.get_transfer_function(freq,signal.device)
+            signal[:] = backend.fft.ifft(backend.fft.fft(signal[:],axis=-1) * self.H)
+            return signal
+
+        return core_real()
+
+    def __call__(self,signal):
+
+        return self.__core(signal)
+
+
+
+class ROADM:
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
