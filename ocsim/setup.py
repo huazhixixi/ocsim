3dce0cef8db7ecb2210d3b44de089521e0866ca9
huazhi <huazhi.lun@sjtu.edu.cn>
2021-02-17 00:51:22 +0800

    add ase noise recorder and osnr recorder to signal

--- ocsim/setup.py
+++ ocsim/setup.py
@@ -3,12 +3,12 @@ import setuptools
 
 setup(
     name="ocsim",
-    version='0.0.7',
+    version='0.0.8',
     author='huazhi lun',
     author_email='huazhi.lun@sjtu.edu.cn',
     description="simulator for optical communications",
     url='https://github.com/huazhixixi/ocsim.git',
     packages=setuptools.find_packages(),
-    install_requires=['prettytable DensityPlot', 'resampy', 'matplotlib',
+    install_requires=['prettytable',' DensityPlot', 'resampy', 'matplotlib',
                       'scipy', 'numpy', 'numba', 'bitarray', 'SciencePlots']
 )
